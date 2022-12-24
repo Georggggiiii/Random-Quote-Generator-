991c4f164b349e52b76c93c392aaa73a0175cef6
giga <giga.elizbarashvili14@gmail.com>
2022-12-24 23:50:51 +0400

    Added new files

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"><br>
+  <button @click="getTodos">Show qoutes</button><br>
+  <div v-for="qoute in qoutes" :key="qoute.id">
+  {{ qoute }}
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      qoutes: []
+    }
+  },
+  methods: {
+    getTodos() {
+      fetch('https://api.kanye.rest/')
+      .then(response => response.json())
+      .then(data => this.qoutes = data)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
